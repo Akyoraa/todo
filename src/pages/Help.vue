e6fe39d1ba1d7fc753658bc0c7a357dee11c30d0
Akyoraa <fillanadya2@gmail.com>
2023-11-20 08:19:17 +0700

    first commit

---
+++ src/pages/Help.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>
+      Welcome to the ToDoList app! We are very happy to help you organize your daily activities more easily and
+      efficiently. On this page, we provide a complete guide on how to use this application. From creating a list of
+      tasks, setting deadlines, marking tasks that have been completed, and setting reminders, we will provide clear
+      step-by-step guidance to ensure you can maximize your experience using ToDoList.
+    </p>
+    <p>
+      If you have any difficulties or questions, please feel free to contact our support team through the help menu or
+      customer service we provide. We are committed to helping you use this app smoothly and get the most out of the
+      features available. Thank you for choosing ToDoList to help you organize your daily activities more efficiently!
+    </p>
+  </q-page>
+</template>
+<script setup>
+import { defineComponent } from "vue";
+
+defineComponent({
+  name: "HelpPage",
+});
+</script>
